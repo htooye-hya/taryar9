b2b062d9e2b259ac13e02be0347ece3c0d4f3922
htooye-hya <htooye@gmail.com>
2026-08-14 09:31:57 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,78 @@
-const CACHE_NAME = 'tara9-pwa-v1';
+const CACHE_NAME = 'tara9-pwa-v2';
+
 const ASSETS = [
   './',
   './index.html',
   './manifest.webmanifest',
   './splash.png',
-  './icons/icon-180.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
   );
+
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(cached =>
-      cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match('./index.html'))
-    )
+    caches.match(event.request)
+      .then(cachedResponse => {
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request)
+          .then(networkResponse => {
+
+            if (
+              !networkResponse ||
+              networkResponse.status !== 200 ||
+              networkResponse.type === 'opaque'
+            ) {
+              return networkResponse;
+            }
+
+            const responseClone = networkResponse.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseClone);
+              });
+
+            return networkResponse;
+          })
+          .catch(() => {
+            if (
+              event.request.mode === 'navigate'
+            ) {
+              return caches.match('./index.html');
+            }
+          });
+      })
   );
 });
